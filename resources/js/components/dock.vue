ff9958506f78970969c971531246a633b96752ab
Aleksandr <sasha.artemev.1001@mail.ru>
2021-09-11 21:17:58 +0300

    update

--- resources/js/components/dock.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>Заголовок</h1>
-</template>
--- resources/js/pages/document.vue
+++ resources/js/pages/document.vue
@@ -1,22 +1,22 @@
 <template>
   <section class="document">
-    <div class="container">
-      <div class="document__wrapper">
-        <div class="document__wrapper-left" v-html="Dock">
+    <div class="document__wrapper">
+      <div class="document__wrapper-left">
+        <div class="dock" v-html="html"></div>
+      </div>
+      <div class="document__wrapper-right">
 
-        </div>
-        <div class="document__wrapper-right"></div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Dock from "../components/dock.vue";
-
 export default {
-  components: {
-    Dock
+  data() {
+    return {
+      html: ``
+    }
   }
-};
+}
 </script>
